c3dea7b3e5839e416c0295b059b0fe59d055232f
honyanglover <jinlee@metamorp.cc>
2019-03-04 06:29:38 +0900

    1

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(80, function () {
+    console.log('Connected 80 port!');
+});
